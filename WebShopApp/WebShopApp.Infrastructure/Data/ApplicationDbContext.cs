9ef1266132b573768a7e1429f54cd77e2a09b843
Stilqn06 <s.gebrev2020@pmg-pernik.com>
2024-12-14 14:11:20 +0200

    Първа част е готова

---
+++ WebShopApp/WebShopApp.Infrastructure/Data/ApplicationDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using WebShopApp;
+using WebShopApp.Infrastructure.Data.Entities;
+
+namespace WebShopApp.Infrastructure.Data;
+
+public class ApplicationDbContext : IdentityDbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+        : base(options)
+    {
+    }
+    public DbSet<Brand> Brands { get; set; }    
+    public DbSet<Product> Products { get;  set; }    
+    public DbSet<Category> Categories { get;  set; } 
+    public DbSet<Order> Orders { get; set; }  
+}
