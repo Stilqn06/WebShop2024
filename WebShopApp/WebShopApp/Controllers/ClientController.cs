7fec126c3c78aa3cca1fa715f2844539f670297b
Stilqn06 <s.gebrev2020@pmg-pernik.com>
2025-01-16 09:32:20 +0200

    8-ma част е готова

---
+++ WebShopApp/WebShopApp/Controllers/ClientController.cs
@@ -0,0 +1,75 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+using WebShopApp.Infrastructure.Data.Entities;
+using WebShopApp.Models.Client;
+
+namespace WebShopApp.Controllers
+{
+    public class ClientController : Controller
+    {
+        private readonly UserManager<ApplicationUser> _userManager;
+        public ClientController(UserManager<ApplicationUser> userManager)
+        {
+            _userManager = userManager;
+        }
+        public async Task<IActionResult> Index()
+        {
+            var allUsers = _userManager.Users.Select(u => new ClientIndexVM
+            {
+                Id = u.Id,
+                UserName = u.UserName,
+                FirstName = u.FirstName,
+                LastName = u.LastName,
+                Address = u.Address,
+                Email = u.Email,
+            }).ToList();
+            var adminIds = (await _userManager.GetUsersInRoleAsync("Administrator")).Select(a => a.Id).ToList();
+            foreach (var user in allUsers)
+            {
+                user.IsAdmin= adminIds.Contains(user.Id);
+            }
+            var users =allUsers.Where(x=>x.IsAdmin==false).OrderBy(x=>x.UserName).ToList();
+            return View(users);
+        }
+        public ActionResult Delete(string id)
+        {
+            var user=_userManager.Users.FirstOrDefault(x=>x.Id==id);
+            if (user == null)
+            {
+                return NotFound();
+            }
+            ClientDeleteVM userToDelete = new ClientDeleteVM()
+            {
+                Id = user.Id,
+                UserName = user.UserName,
+                FirstName = user.FirstName,
+                LastName = user.LastName,
+                Address = user.Address,
+                Email = user.Email,
+            };
+            return View(userToDelete);
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Delete(ClientDeleteVM bindingModel)
+        {
+            string id = bindingModel.Id;
+            var user =await _userManager.FindByIdAsync(id);
+            if (user == null)
+            {
+                return NotFound();
+            }
+            IdentityResult result=await _userManager.DeleteAsync(user);
+            if (result.Succeeded)
+            {
+                return RedirectToAction("Success");
+            }
+            return NotFound();
+        }
+        public ActionResult Success()
+        {
+            return View();
+        }
+    }
+}
